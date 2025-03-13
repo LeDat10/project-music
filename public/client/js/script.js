4792f296c79308e0e8725c85962edf53041c23b9
ledat <ledat1010204@gmail.com>
2025-03-13 15:32:52 +0700

    Làm trang chi tiết bài hát

--- public/client/js/script.js
+++ public/client/js/script.js
@@ -1,1 +1,35 @@
-console.log("OK");
+const aplayer = document.querySelector("#aplayer");
+
+if (aplayer) {
+    let dataSong = aplayer.getAttribute("data-song");
+
+    dataSong = JSON.parse(dataSong);
+
+    let dataSinger = aplayer.getAttribute("data-singer");
+
+    dataSinger = JSON.parse(dataSinger);
+
+
+    const ap = new APlayer({
+        container: aplayer,
+        audio: [{
+            name: dataSong.title,
+            artist: dataSinger.fullName,
+            url: dataSong.audio,
+            cover: dataSong.avatar,
+        }],
+        autoplay: true
+    });
+
+    const avatar = document.querySelector(".singer-detail .inner-avatar");
+    ap.on('pause', () => {
+        avatar.style.animationPlayState = "paused";
+    });
+
+    ap.on('play', () => {
+        avatar.style.animationPlayState = "running";
+    });
+}
+
+
+
